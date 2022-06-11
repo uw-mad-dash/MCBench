92a250155123cb4d8d1e754b41ff125a3b6e64b0
waterpine <songbian@zju.edu.cn>
2022-06-10 21:40:12 -0700

    split checkpoint into model parallelism

---
+++ tools/test.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+TARGET_TENSOR_MODEL_PARALLEL_SIZE=2
+TARGET_PIPELINE_MODEL_PARALLEL_SIZE=2
+
+VOCAB_FILE=../bert-large-cased-vocab.txt
+CHECKPOINT_PATH=../examples/checkpoints/bert_345m
+
+WORLD_SIZE=4
+
+DISTRIBUTED_ARGS="--nproc_per_node $WORLD_SIZE \
+                  --nnodes 1 \
+                  --node_rank 0 \
+                  --master_addr localhost \
+                  --master_port 6000"
+
+
+python -m torch.distributed.launch $DISTRIBUTED_ARGS split_mp_partitions.py \
+        --model-type BERT \
+        --pipeline-model-parallel-size 2 \
+        --tensor-model-parallel-size 2 \
+        --target-tensor-model-parallel-size $TARGET_TENSOR_MODEL_PARALLEL_SIZE \
+        --target-pipeline-model-parallel-size $TARGET_PIPELINE_MODEL_PARALLEL_SIZE \
+        --tokenizer-type BertWordPieceLowerCase \
+        --vocab-file $VOCAB_FILE \
+        --num-layers 24 \
+        --hidden-size 1024 \
+        --num-attention-heads 16 \
+        --seq-length 512 \
+        --max-position-embeddings 512 \
+        --load $CHECKPOINT_PATH \
+        --save $CHECKPOINT_PATH/split \
+        --task MNLI \
+        --micro-batch-size 1 \
+        --save-interval 500000
---
+++ tools/test_uae.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+#SBATCH --job-name=split    # create a short name for your job
+#SBATCH --output=split.txt
+#SBATCH --nodes=1                # node count
+#SBATCH --ntasks-per-node=4      # total number of tasks across all nodes
+#SBATCH --cpus-per-task=16        # cpu-cores per task (>1 if multi-threaded tasks)
+#SBATCH --mem=64G                 # total memory per node (4 GB per cpu-core is default)
+#SBATCH --gres=gpu:4              # number of gpus per node
+#SBATCH --time=202:00:00          # total run time limit (HH:MM:SS)
+
+TARGET_TENSOR_MODEL_PARALLEL_SIZE=2
+TARGET_PIPELINE_MODEL_PARALLEL_SIZE=2
+
+VOCAB_FILE=../bert-large-cased-vocab.txt
+CHECKPOINT_PATH=../examples/checkpoints/bert_345m
+
+WORLD_SIZE=4
+
+DISTRIBUTED_ARGS="--nproc_per_node $WORLD_SIZE \
+                  --nnodes 1 \
+                  --node_rank 0 \
+                  --master_addr localhost \
+                  --master_port 6000"
+
+
+python -m torch.distributed.launch $DISTRIBUTED_ARGS split_mp_partitions.py \
+        --model-type BERT \
+        --pipeline-model-parallel-size 2 \
+        --tensor-model-parallel-size 2 \
+        --target-tensor-model-parallel-size $TARGET_TENSOR_MODEL_PARALLEL_SIZE \
+        --target-pipeline-model-parallel-size $TARGET_PIPELINE_MODEL_PARALLEL_SIZE \
+        --tokenizer-type BertWordPieceLowerCase \
+        --vocab-file $VOCAB_FILE \
+        --num-layers 24 \
+        --hidden-size 1024 \
+        --num-attention-heads 16 \
+        --seq-length 512 \
+        --max-position-embeddings 512 \
+        --load $CHECKPOINT_PATH \
+        --save $CHECKPOINT_PATH/split_16 \
+        --task MNLI \
+        --micro-batch-size 1 \
+        --save-interval 500000
